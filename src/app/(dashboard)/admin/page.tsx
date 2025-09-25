ba2680e151c7f66b847c21f9ae7295003d63a892
UKOBIZABA Aimable <154277022+AimableTrague@users.noreply.github.com>
2025-09-25 14:11:46 +0200

    chore: Worked with aligning the components well and static to dynamic data change

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,11 +1,15 @@
 import Announcements from "@/components/Announcements";
 import AttendanceChartContainer from "@/components/AttendanceChartContainer";
 import CountChartContainer from "@/components/CountChartContainer";
-import EventCalendar from "@/components/EventCalendar";
+import EventCalendarContainer from "@/components/EventCalendarContainer";
 import FinanceChart from "@/components/FinanceChart";
 import UserCard from "@/components/UserCard";
 
-const AdminPage = () => {
+const AdminPage = async ({
+  searchParams,
+}: {
+  searchParams: { [keys: string]: string | undefined };
+}) => {
   return (
     <div className="flex p-4 gap-4 flex-col md:flex-row">
       <div className="w-full lg:w-2/3 flex flex-col gap-5">
@@ -36,7 +40,7 @@ const AdminPage = () => {
       </div>
       {/* right */}
       <div className="w-full lg:w-1/3 flex flex-col gap-8">
-        <EventCalendar />
+        <EventCalendarContainer searchParams={searchParams} />
         <Announcements />
       </div>
     </div>
